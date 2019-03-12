fd1963c39fe7cc2cfcec92c1aaa68d7620658be5
zacharyranes <zachary.mt.ranes@gmail.com>
2019-03-12 15:55:01 -0400

    work done on ranes cpu

--- Example_2.5 standalone.py
+++ Example_2.5 standalone.py
@@ -4,8 +4,7 @@ import docx
 
 import re
 
-docLength = 0
-validPages = 0
+validInstances = 0
 
 #Opens Document
 def openDoc():
@@ -23,22 +22,41 @@ def DocumentMaterial(Doc):
 
 #Searches current page for IPs
 def findIPs(document):
-    global validPages
+    global validInstances
     testableDoc = document
-    DesiredIP = "192."#re.compile("%03d" + '.' "%03d" + '.' "%03d" + '.' "%03d") #"192.168.0.1"# #("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$") 
-    #print(DesiredIP)
-    #print(testableDoc)
-    for i in range(0,len(testableDoc)-3):
-        if testableDoc[i:i+4]==DesiredIP:#.match(document[i:i+15]):
-            validPages+=1#print(pdf[i:i+15])
-            #return "true"
- 
+    #print("Test Flag 1")
+    for i in range(0,len(testableDoc)-14):
+        print("Testing...")
+        for a in range(192,193):
+            for b in range(0,20):
+               #print(validInstances)
+                #print("Testing IPs in range of: " + str(a) +"." + str(b) + ".###.###")
+                for c in range(0,20):
+                    for d in range(0,120):
+                        for k in range(7,15):
+                            #print("Pass " + str(k-6) + "/9 completed.")
+                            #print("Testing for: " + str(a) + "." + str(b) + "." + str(c) + "."+ str(d))
+                            if testableDoc[i:i+k] == str(str(a) + "." + str(b) + "." + str(c) + "." + str(d)):
+                                #print("Instance found!")
+                                validInstances+=1 #print(pdf[i:i+15])
+                                print('Instance found!')
+                                print(validInstances)
+
+def findIPs2(document):
+    global validInstances
+    testableDoc = document
+    DesiredIP = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+        for i in range(0,len(testableDoc)-6):
+            if DesiredIP.match(testableDoc[i:i+):
+                validInstances+=1
+                #print("Instance found!")
 #Main Program        
 X = openDoc()
 Y = DocumentMaterial(X)
-findIPs(Y)
-#print(validPages)
+#findIPs(Y)
+findIPs2(Y)
+print(validInstances)
 
         
 #print()
-print("There are " + str(validPages) + " instances of IPs found.")
+#print("There are " + str(validInstances) + " instances of IPs found.")
